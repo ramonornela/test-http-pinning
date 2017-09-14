d6c10b282ebd5f304b1eec3af7f0d74e495ca4c4
Ramon Henrique Ornelas <ramon.ornela@gmail.com>
2017-09-14 13:34:56 -0300

    chore(*): add body-parser middleware express

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 #! /usr/env/node
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.get('/test-get', (req, res) => {
   console.log(req.query);
   res.send('Foo');
+});
+
+app.post('/test-post', (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
 });
 
 app.listen(3000);
