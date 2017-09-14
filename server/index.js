fd71120ff8ad0b60d7e9f8be3f169c719230eb17
Ramon Henrique Ornelas <ramon.ornela@gmail.com>
2017-09-14 16:22:47 -0300

    chore(*): fix middleware body-parser

--- server/index.js
+++ server/index.js
@@ -3,14 +3,14 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
+const urlencoded = bodyParser.urlencoded({ extended: false })
 
 app.get('/test-get', (req, res) => {
   console.log(req.query);
   res.send('Foo');
 });
 
-app.post('/test-post', (req, res) => {
+app.post('/test-post', urlencoded, (req, res) => {
   console.log(req.body);
   res.send('Ok');
 });
